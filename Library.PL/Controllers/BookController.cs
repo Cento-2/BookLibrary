7c162a3695dee4e3b5a00bc92d7c6fe1defc9f9d
Cento-2 <mattia.centonze2005@gmail.com>
2026-05-29 13:58:34 +0200

    Mancano le razorpage

--- Library.PL/Controllers/BookController.cs
+++ Library.PL/Controllers/BookController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Library.BLL.Interfaces;
+using Library.BLL.Models;
+using Library.DAL.Entities;
+using Library.PL.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Library.PL.Controllers
 {
     public class BookController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericService<Book, BookModel> _service;
+        private readonly IMapper _mapper;
+
+        public BookController(IGenericService<Book, BookModel> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _service.GetAllAsync();
+            var vm = _mapper.Map<IEnumerable<BookViewModel>>(books);
+            return View(vm);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BookViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var model = _mapper.Map<BookModel>(vm);
+            await _service.AddAsync(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<BookViewModel>(model);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BookViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var model = _mapper.Map<BookModel>(vm);
+            await _service.UpdateAsync(vm.Id, model);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<BookViewModel>(model);
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<BookViewModel>(model);
+            return View(vm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _service.DeleteAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- Library.PL/Controllers/LoanController.cs
+++ Library.PL/Controllers/LoanController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Library.BLL.Interfaces;
+using Library.BLL.Models;
+using Library.DAL.Entities;
+using Library.PL.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Library.PL.Controllers
 {
     public class LoanController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericService<Loan, LoanModel> _service;
+        private readonly IMapper _mapper;
+
+        public LoanController(IGenericService<Loan, LoanModel> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var loans = await _service.GetAllAsync();
+            var vm = _mapper.Map<IEnumerable<LoanViewModel>>(loans);
+            return View(vm);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(LoanViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var model = _mapper.Map<LoanModel>(vm);
+            await _service.AddAsync(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<LoanViewModel>(model);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(LoanViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var model = _mapper.Map<LoanModel>(vm);
+            await _service.UpdateAsync(vm.Id, model);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<LoanViewModel>(model);
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            var vm = _mapper.Map<LoanViewModel>(model);
+            return View(vm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _service.DeleteAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
