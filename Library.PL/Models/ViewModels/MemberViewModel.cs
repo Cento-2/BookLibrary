b3eb3411263f4a636cdbb6bfcbe48dcc5415ceb2
Cento-2 <mattia.centonze2005@gmail.com>
2026-05-29 14:43:20 +0200

    Aggiustato

--- Library.PL/Models/ViewModels/MemberViewModel.cs
+++ Library.PL/Models/ViewModels/MemberViewModel.cs
@@ -1,14 +1,14 @@
 ﻿using Library.BLL.Models;
+using Library.DAL.Entities;
 
 namespace Library.PL.Models.ViewModels
 {
     public class MemberViewModel
     {
         public int Id { get; set; }
-        public DateTime LoanDate { get; set; }
-        public double TicketPrice { get; set; }
-        public int BookId { get; set; }
-        public BookModel Book { get; set; }
-        public int MemberId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
     }
 }
