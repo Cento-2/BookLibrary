3839e5cb62c48d735aa3e4ab5fbcfe4f8724e178
jon19dothtml <jojodonghia2005@gmail.com>
2026-05-29 16:21:30 +0200

    fix JONNY

---
+++ Library.PL/Pages/Loans/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.PL.Pages.Loans
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Library.PL/Pages/Members/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.PL.Pages.Members
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
